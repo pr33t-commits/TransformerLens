7a00dcf0fb860c3feb5a7729aae44919101a5a46
pr33t-commits <pdlodaya@gmail.com>
2025-08-17 08:51:16 +0530

    Update llama.py

--- transformer_lens/pretrained/weight_conversions/llama.py
+++ transformer_lens/pretrained/weight_conversions/llama.py
@@ -25,8 +25,10 @@ def convert_llama_weights(llama, cfg: HookedTransformerConfig):
     # GPTNeoX with different names
 
     assert cfg.d_mlp is not None  # keep mypy happy
-
+    if hasattr(llama.config, "num_hidden_layers"):
+        print(f"Number of hidden layers :- {llama.config.num_hidden_layers}")
     for l in range(cfg.n_layers):
+        print(f'Layer :- {l}')
         state_dict[f"blocks.{l}.ln1.w"] = llama.model.layers[l].input_layernorm.weight
 
         W_Q = llama.model.layers[l].self_attn.q_proj.weight
